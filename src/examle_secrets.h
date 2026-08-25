6ba4efa26902401bd178a929015588d409efc5cc
franc <venturaperezfrancisco1@gmail.com>
2026-08-25 05:26:18 -0600

    Example secrets

---
+++ src/examle_secrets.h
@@ -0,0 +1,11 @@
+//
+// Created by franc on 25/08/2026.
+//
+
+#ifndef ENLATADORAIOT_SECRETS_H
+#define ENLATADORAIOT_SECRETS_H
+// --- Wi-Fi Credentials ---
+auto ssid = "YOUR_WIFI_SSID";
+auto password = "YOUR_WIFI_PASSWORD";
+
+#endif //ENLATADORAIOT_SECRETS_H
